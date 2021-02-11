89bafea7f935740427c77a81a878d04e5a2d66b1
drdevlin <drdevlin@fastmail.com>
2021-02-11 16:21:45 -0500

    server: add cors

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const root = require('./routers/root.js');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/', root);
 
