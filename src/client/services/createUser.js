fa8a7e0521b36b72c464105334adb2ac13aa78b0
drdevlin <drdevlin@fastmail.com>
2021-02-11 16:21:28 -0500

    client: connect createUser to server

--- src/client/services/createUser.js
+++ src/client/services/createUser.js
@@ -2,11 +2,16 @@ const createUser = async (user) => {
   try {
     const response = await fetch('http://localhost:4545/users', { 
       method: 'POST',
-      body: JSON.stringify(user)
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify([user])
     });
-    return { success: true, message: response };
+    if (response.ok) {
+      return response;
+    } else {
+      throw new Error(response.statusText);
+    }
   } catch(error) {
-    return { success: false, message: error.message };
+    return error.message;
   }
 }
 
--- src/client/services/createUser.test.js
+++ src/client/services/createUser.test.js
@@ -4,19 +4,16 @@ describe('createUser(userInfo)', () => {
   it('returns a response', async () => {
     const user = { email: 'foo@bar.com', password: 'baz' };
     const response = await createUser(user);
-    console.log(response);
     expect(response).not.toBeNull();
   });
   it('returns a successful status object on success', async () => {
-    const user = { email: 'foo@bar.com', password: 'baz' };
-    const expectedResponse = { success: true, message: 'User foo@bar.com created.' };
-    const actualResponse = await createUser(user);
-    expect(actualResponse).toMatchObject(expectedResponse);
+    const user = { email: 'goo@car.com', password: 'caz' };
+    const response = await createUser(user);
+    expect(response.ok).toBeTruthy();
   });
   it('returns a failure status object on fail', async () => {
-    const user = { email: 'foo@bar.com', password: 'baz' };
+    const user = { email: 'goo@car.com', password: 'caz' };
     const response = await createUser(user);
-    expect(response.success).toBeFalsy();
-    expect(response.message).toBeTruthy();
+    expect(response.ok).toBeFalsy();
   });
 });
