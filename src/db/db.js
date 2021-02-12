cd962e287ed0d2fdafca91e375fbd436ca277cf8
drdevlin <drdevlin@fastmail.com>
2021-02-12 13:47:17 -0500

    db: rough in crud functions

--- src/db/db.js
+++ src/db/db.js
@@ -5,12 +5,58 @@ const schemas = require('./schemas.js');
 const pool = new Pool();
 
 const create = (table, rows) => {
-  const schemaKeys = Object.keys(schemas[table]);
-  const columns = '(' + schemaKeys.join(', ') + ')';
+  if (table && rows) {
+    const schemaKeys = Object.keys(schemas[table]);
+    const columns = '(' + schemaKeys.join(', ') + ')';
 
-  const rowsValues = rows.map(row => Object.values(row));
+    const rowsValues = rows.map(row => Object.values(row));
 
-  return Promise.all(rowsValues.map(values => pool.query(`INSERT INTO ${table} ${columns} VALUES ($1, $2)`, values)));
+    return Promise.all(rowsValues.map(values => pool.query(`INSERT INTO ${table} ${columns} VALUES ($1, $2)`, values)));
+  } else {
+    return Promise.reject('Faulty call to function');
+  }
+  
 };
 
-module.exports = { create };
+const read = (table, conditions = '') => {
+  if (table) {
+    const conditionStatement = ` WHERE ${conditions}`;
+    return new Promise(resolve => resolve(pool.query(`SELECT * FROM ${table}${conditionStatement}`)));
+  } else {
+    return Promise.reject('Faulty call to function');
+  }
+};
+
+const update = (table, rowIdentifier, data) => {
+  if (table && rowIdentifier && data) {
+    let [ identifierKey, identifierValue ] = Object.entries(rowIdentifier)[0];
+    if (typeof identifierValue === 'string') identifierValue = `'${identifierValue}'`;
+    const conditionStatement = ` WHERE ${identifierKey} = ${identifierValue}`;
+  
+    let setStatement = ' SET ';
+    for (let [ key, value ] of Object.entries(data)) {
+      if (typeof value === 'string') value = `'${value}'`;
+      setStatement += `${key} = ${value},`;
+    }
+    setStatement = setStatement.slice(0, -1);
+   
+    return new Promise(resolve => resolve(pool.query(`UPDATE ${table}${setStatement}${conditionStatement}`)));
+  } else {
+    return Promise.reject('Faulty call to function');
+  }
+}
+
+const del = (table, rowIdentifier) => {
+  if (table && rowIdentifier) {
+    let [ identifierKey, identifierValue ] = Object.entries(rowIdentifier)[0];
+    if (typeof identifierValue === 'string') identifierValue = `'${identifierValue}'`;
+    const conditionStatement = ` WHERE ${identifierKey} = ${identifierValue}`;
+
+    return new Promise(resolve => resolve(pool.query(`DELETE FROM ${table}${conditionStatement}`)));
+  } else {
+    return Promise.reject('Faulty call to function');
+  }
+}
+
+
+module.exports = { create, read, update, del };
--- src/db/db.test.js
+++ src/db/db.test.js
@@ -1,18 +1,43 @@
 const db = require('./db.js');
 
-describe('create(table, rows)', () => {
-  it('returns something', async () => {
-    const result = await db.create('users', [{ email: 'foo@bar.com', password: 'baz' }]);
-    expect(result).toBeTruthy();
+describe('db', () => {
+  describe('create(table, rows)', () => {
+    it('returns something', async () => {
+      const result = await db.create('users', [{ email: 'foo@bar.com', password: 'baz' }]);
+      expect(result).toBeTruthy();
+    });
+    it('creates a single row in Postgres db', async () => {
+      const result = await db.create('users', [{ email: 'goo@car.com', password: 'caz' }]);
+      expect(result[0].rowCount).toBe(1);
+    });
+    it('creates multiple rows in Postgres db', async () => {
+      const results = await db.create('users', [{ email: 'hoo@dar.com', password: 'daz' }, { email: 'joo@far.com', password: 'faz' }]);
+      const rowCounts = results.map(({ rowCount }) => rowCount );
+      const completeSuccess = rowCounts.every(rowCount => rowCount === 1);
+      expect(completeSuccess).toBeTruthy();
+    });
   });
-  it('creates a single row in Postgres db', async () => {
-    const result = await db.create('users', [{ email: 'goo@car.com', password: 'caz' }]);
-    expect(result[0].rowCount).toBe(1);
+  describe('read(table, conditions)', () => {
+    it('returns all columns under the specified conditions', async () => {
+      const expectedResults = [{ email: 'foo@bar.com', password: 'baz' }];
+      const actualResults = await db.read('users', "email = 'foo@bar.com'");
+      expect(actualResults.rows).toMatchObject(expectedResults);
+    });
   });
-  it('creates multiple rows in Postgres db', async () => {
-    const results = await db.create('users', [{ email: 'hoo@dar.com', password: 'daz' }, { email: 'joo@far.com', password: 'faz' }]);
-    const rowCounts = results.map(({ rowCount }) => rowCount );
-    const completeSuccess = rowCounts.every(rowCount => rowCount === 1);
-    expect(completeSuccess).toBeTruthy();
+  describe('update(table, rowIdentifier, data', () => {
+    it('updates one cell at the row identified', async () => {
+      const result = await db.update('users', { email: 'foo@bar.com' }, { password: 'updated!' });
+      expect(result.rowCount).toBe(1);
+    });
+    it('updates multiple cells at the row identified', async () => {
+      const result = await db.update('users', { email: 'goo@car.com' }, { email: 'updated!', password: 'updated!' });
+      expect(result.rowCount).toBe(1);
+    });
   });
-})
+  describe('del(table, rowIdentifier', () => {
+    it('deletes the row identified', async () => {
+      const result = await db.del('users', { email: 'joo@far.com' });
+      expect(result.rowCount).toBe(1);
+    })
+  })
+});
