0df03206e526db13b870723e51eb29c3c25fc4af
drdevlin <drdevlin@fastmail.com>
2021-02-13 13:47:24 -0500

    db: update schemas

--- src/db/schemas.js
+++ src/db/schemas.js
@@ -1,6 +1,51 @@
 const users = {
-  email: 'string',
-  password: 'string'
+  email: 'string', //max length 48
+  password: 'string' //max length 48
 };
 
-module.exports = { users };
+const documents = {
+  id: 'string', //uuid
+  title: 'string', //max length 48
+  user_id: 'string' //fk users email
+}
+
+const theses = {
+  id: 'string', //uuid
+  thesis: 'string', //max length 256
+  document_id: 'string' //fk documents id
+}
+
+const supporting_claims = {
+  id: 'string', //uuid
+  claim: 'string', //max length 256
+  position: 'number',
+  document_id: 'string' //fk documents id
+}
+
+const clarifying_sentences = {
+  id: 'string', //uuid
+  sentence: 'string', //max length 256
+  supporting_claim_id: 'string' //fk supporting_claims id
+}
+
+const examples = {
+  id: 'string', //uuid
+  example: 'string', //max length 256
+  supporting_claim_id: 'string' //fk supporting_claims id
+}
+
+const linking_sentences = {
+  id: 'string', //uuid
+  sentence: 'string', //max length 256
+  supporting_claim_id: 'string' //fk supporting_claims id
+}
+
+module.exports = { 
+  users, 
+  documents, 
+  theses,
+  supporting_claims,
+  clarifying_sentences,
+  examples,
+  linking_sentences
+};
